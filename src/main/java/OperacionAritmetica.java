0c2d0f8622c46030cc722c3e63668ae1066a336e
Luis Fer <pastillalc@gmail.com>
2018-06-05 08:52:47 -0400

    Luis Choque merge con master

--- src/main/java/OperacionAritmetica.java
+++ src/main/java/OperacionAritmetica.java
@@ -1,22 +1,22 @@
 import java.math.BigDecimal;
 
 public class OperacionAritmetica {
-
     /*
         metodo multiplicaion
         el siguiente metodo recibe 2 numeros double y devuelve un double
      */
 
-    public static double multiplicaion(double x,double y){
+    public static double multiplicaion(double x,double y) {
         BigDecimal bd1 = new BigDecimal(x);
         BigDecimal bd2 = new BigDecimal(y);
         return bd1.multiply(bd2).doubleValue();
     }
-    public static double suma(double a, double b) { return a + b; }
-
-        public static double resta(double a, double b){
-        double c = a - b;
-        return c;
+        public static double suma ( double a, double b){
+            return a + b;
         }
-    public static double suma(double a, double b) { return a + b; }
-}
+
+        public static double resta ( double a, double b){
+            double c = a - b;
+            return c;
+        }
+    }
