7269ee211befd57130247a889d3495e017306b3f
x4tkc <tovilaskevin@gmail.com>
2018-06-05 08:15:36 -0400

    division-k

---
+++ src/main/java/OperacionAritmetica.java
@@ -0,0 +1,10 @@
+public class OperacionAritmetica {
+    public static void main(String[]args){
+        System.out.println("division");
+        System.out.println(div(3,4));
+    }
+    public static double div(double x, double y){
+        return x/y;
+    }
+
+}
