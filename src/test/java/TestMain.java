2c468328f09cba6b101a4ce0043b188fa87d0687
x4tkc <tovilaskevin@gmail.com>
2018-06-05 08:34:20 -0400

    division-k.2

--- src/test/java/TestMain.java
+++ src/test/java/TestMain.java
@@ -19,16 +19,16 @@ public class TestMain {
 
 
     }
-    @Test
-    public void verifcarResultadoIncorrecto(){
-        double expectedResultIncorrect = -10.0/-50.0;
-
-
-
-        double actualResultIncorrect = OperacionAritmetica.div(-10,-50);
-
-        Assert.assertEquals(expectedResultIncorrect, actualResultIncorrect,0.002);
-    }
+//    @Test
+//    public void verifcarResultadoIncorrecto(){
+//        double expectedResultIncorrect = -10.0/-50.0+2;
+//
+//
+//
+//        double actualResultIncorrect = OperacionAritmetica.div(-10,-50);
+//
+//        Assert.assertEquals(expectedResultIncorrect, actualResultIncorrect,0.002);
+//    }
 
 
 }
