cf21af8d98f1832a2c4637bb38b167ab1a7426c7
Luis Fer <pastillalc@gmail.com>
2018-06-05 08:44:45 -0400

    Luis Choque metodo multiplicacion y unit test

--- src/main/java/OperacionAritmetica.java
+++ src/main/java/OperacionAritmetica.java
@@ -7,15 +7,13 @@ public class OperacionAritmetica {
 
     /*
         metodo multiplicaion
-        el siguiente metodo recibe 2 numeros entero s
+        el siguiente metodo recibe 2 numeros double y devuelve un double
      */
 
     public static double multiplicaion(double x,double y){
-
         BigDecimal bd1 = new BigDecimal(x);
         BigDecimal bd2 = new BigDecimal(y);
-
         return bd1.multiply(bd2).doubleValue();
     }
-
+    public static double suma(double a, double b) { return a + b; }
 }
